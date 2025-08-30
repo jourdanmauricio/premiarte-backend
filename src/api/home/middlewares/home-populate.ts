a9fae3364014f2c454386af0063d5ceaf87b185a
mauricio <jourdanmauricio@gmail.com>
2025-08-30 11:38:01 -0300

    Agrego middleware slider

---
+++ src/api/home/middlewares/home-populate.ts
@@ -0,0 +1,41 @@
+/**
+ * `home-populate` middleware
+ */
+
+import type { Core } from '@strapi/strapi';
+
+export default (config, { strapi }: { strapi: Core.Strapi }) => {
+  // Add your own logic here.
+  return async (ctx, next) => {
+    strapi.log.info('In home-populate middleware.');
+
+    // Configuración para el populate en Strapi v5
+    ctx.query = {
+    	...ctx.query, // Mantén los query params existentes
+      
+    	populate: {
+      	cover: {
+          fields: ['name', 'width', 'height', 'alternativeText', 'url'], 
+        },
+         slider: {
+           populate: {
+              button: true,
+             card: {
+               fields: ['title', 'description'],
+               populate: {
+                 image: {
+           	     	fields: ['name', 'width', 'height', 'alternativeText', 'url']
+               	}
+               } 
+            	}
+           }
+         }
+      },
+      fields: ['title', 'description', 'categoriesTitle', 'productsTitle'], 
+    }
+
+    strapi.log.debug('Modified query:', ctx.query); // Debug: Verifica el query modificado
+
+    await next();
+  };
+};
--- src/api/home/routes/home.ts
+++ src/api/home/routes/home.ts
@@ -4,4 +4,10 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::home.home');
+export default factories.createCoreRouter('api::home.home', {
+  config: {
+    find: {
+      middlewares: ['api::home.home-populate'],
+    }  
+  } 
+});
