35d494608ac07f737a5582d5a6427d16af015db4
mauricio <jourdanmauricio@gmail.com>
2025-09-05 07:30:27 -0300

    Agrego findOne por products/:slug

--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -4,4 +4,21 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::product.product');
+export default factories.createCoreController('api::product.product', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    
+    // Buscar por slug en lugar de ID
+    const entity = await strapi.db.query('api::articulo.articulo').findOne({
+      where: { slug: id },
+      populate: ['imagen', 'categoria'] // Ajusta los campos que necesites
+    });
+
+    if (!entity) {
+      return ctx.notFound('Artículo no encontrado');
+    }
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+    return this.transformResponse(sanitizedEntity);
+  }
+}));
--- src/api/product/routes/product.ts
+++ src/api/product/routes/product.ts
@@ -6,6 +6,11 @@ import { factories } from '@strapi/strapi';
 
 export default factories.createCoreRouter('api::product.product', {
   config: {
+    findOne: {
+      auth: false, // Opcional: configurar autenticación
+      policies: [],
+      middlewares: [],
+    },
     find: {
       middlewares: ['api::product.products-populate'],
     }  
