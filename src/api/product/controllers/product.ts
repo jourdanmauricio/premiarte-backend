64476b0a140bd413553f2c569cbce454bff4820f
mauricio <jourdanmauricio@gmail.com>
2025-09-05 07:42:16 -0300

    .

--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -7,15 +7,22 @@ import { factories } from '@strapi/strapi'
 export default factories.createCoreController('api::product.product', ({ strapi }) => ({
   async findOne(ctx) {
     const { id } = ctx.params;
-    
+
     // Buscar por slug en lugar de ID
-    const entity = await strapi.db.query('api::articulo.articulo').findOne({
+    const entity = await strapi.db.query('api::product.product').findOne({
       where: { slug: id },
-      populate: ['imagen', 'categoria'] // Ajusta los campos que necesites
+      populate: {
+        images: {
+          fields: ['alternativeText', 'url'], 
+        },
+        categories: {
+          fields: ['name', 'slug']
+        }
+      }
     });
 
     if (!entity) {
-      return ctx.notFound('Artículo no encontrado');
+      return ctx.notFound('Producto no encontrado');
     }
 
     const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
