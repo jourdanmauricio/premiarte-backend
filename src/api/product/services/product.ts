524a1f3a406c0307584b89c9984df7011cc25b00
mauricio <jourdanmauricio@gmail.com>
2025-09-07 10:19:15 -0300

    .

--- src/api/product/services/product.ts
+++ src/api/product/services/product.ts
@@ -7,7 +7,7 @@ import { factories } from '@strapi/strapi';
 export default factories.createCoreService('api::product.product', ({ strapi }) => ({
   async findBySlug(slug: string) {
     return await strapi.db.query('api::product.product').findOne({
-      where: { slug },
+      where: { slug, publishedAt: { $notNull: true } },
       populate: {
         images: {
           fields: ['alternativeText', 'url'], 
