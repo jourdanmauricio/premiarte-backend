059ac66b0faed6d890eb7882ded06889c103af75
mauricio <jourdanmauricio@gmail.com>
2026-02-21 18:14:27 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -48,6 +48,7 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
